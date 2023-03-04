4d35c5b83a20a997f225928e377da5deeb115093
Holly <holly.cg@outlook.com>
2023-03-04 00:30:14 -0600

    new

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,29 @@
-package org.example;public class Main {
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        ArrayList<String>yoda = new ArrayList<String>();
+        yoda.add("Do.");
+        yoda.add("Or.");
+        yoda.add("do");
+        yoda.add("not");
+        yoda.add("there");
+        yoda.add("is");
+
+       /* Iterator<String> i = yoda.iterator(); //con un iterador
+            while(i.hasNext()){
+                System.out.println(i.next());
+            }*/
+       /* System.out.println("LAMBDA"); //con lambda
+        yoda.forEach((ott)->{System.out.println(ott);});*/
+
+        System.out.println("otro metodo: ");
+        for(String n: yoda){
+            System.out.println(n);
+        }
     }
 }
