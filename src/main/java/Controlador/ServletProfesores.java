75ca163038b69672529287de93060de4d261a004
Holly <holly.cg@outlook.com>
2023-04-29 17:45:15 -0600

    nueva modificacion

---
+++ src/main/java/Controlador/ServletProfesores.java
@@ -0,0 +1,24 @@
+package Controlador;
+
+import Datos.ProfesoresDAO;
+import Modelo.HAHP.Profesores;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+@WebServlet("/ServletProfesores")
+public class ServletProfesores extends HttpServlet {
+        private static final long serialVersionUID = 1L;
+        @Override
+        protected void doGet(HttpServletRequest hsreq, HttpServletResponse hsres)
+                throws ServletException, IOException {
+            List<Profesores> profesores = new ProfesoresDAO().listar();
+            System.out.println("profesores = " + profesores);
+            hsreq.setAttribute("profesores", profesores);
+            hsreq.getRequestDispatcher("profesores.jsp").forward(hsreq, hsres);
+        }
+}
---
+++ src/main/java/Datos/Conexion.java
@@ -0,0 +1,62 @@
+package Datos;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Conexion {
+    private static String user = "postgres";
+    private static String pswd = "12345";
+    private static String bd = "Escuela";
+    private static String server = "jdbc:postgresql://localhost:5432/" + bd;
+    private static String driver = "org.postgresql.Driver";
+
+
+    private static BasicDataSource ds;
+
+    public static DataSource getDataSource() {
+        if (ds == null) {
+            ds = new BasicDataSource();
+            ds.setUrl(server);
+            ds.setUsername(user);
+            ds.setPassword(pswd);
+            ds.setInitialSize(50);
+            ds.setDriverClassName(driver);
+        }
+        return ds;
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return getDataSource().getConnection();
+    }
+
+    public static void close(ResultSet rs) {
+        try {
+            rs.close();
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        }
+    }
+
+    public static void close(PreparedStatement ps) {
+        try {
+            ps.close();
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        }
+    }
+
+    public static void close(Connection conn) {
+        try {
+            conn.close();
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        }
+    }
+
+}
+
